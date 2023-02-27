d7f4c022ea633d9222c75af1a459db4e8e4f42f9
kLaz3r <klazer220033@gmail.com>
2023-02-27 22:52:05 +0200

    added favicon and PWA

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+const withPWA = require('next-pwa')({
+    dest: 'public',
+});
+const nextConfig = {};
+
+module.exports = withPWA(nextConfig);
